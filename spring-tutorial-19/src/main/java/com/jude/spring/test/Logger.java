d5a6e3505d91c528e7466b5b51c4763126ab4fc7
jude <jude@gogame.net>
2015-10-04 23:20:27 +0800

    autowiring by type

---
+++ spring-tutorial-19/src/main/java/com/jude/spring/test/Logger.java
@@ -0,0 +1,25 @@
+package com.jude.spring.test;
+
+/**
+ * Created by judetan on 4/10/15.
+ */
+public class Logger {
+    private ConsoleWriter consoleWriter;
+    private FileWriter fileWriter;
+
+    public void setConsoleWriter(ConsoleWriter consoleWriter) {
+        this.consoleWriter = consoleWriter;
+    }
+
+    public void setFileWriter(FileWriter fileWriter) {
+        this.fileWriter = fileWriter;
+    }
+
+    public void writeFile(String text) {
+        fileWriter.write(text);
+    }
+
+    public void writeConsole(String text) {
+        consoleWriter.write(text);
+    }
+}
