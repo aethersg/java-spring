5d10b8ce248c2a770aa06606463265810fad9c46
jude <jude@gogame.net>
2015-10-04 18:42:10 +0800

    changed in application so that it can find the resource it needs in the class path for however for some systems it will need to find it defined in the resource

--- spring-tutorial-6/src/main/java/com/jude/test/spring/App.java
+++ spring-tutorial-6/src/main/java/com/jude/test/spring/App.java
@@ -1,6 +1,7 @@
-package com.jude.test;
+package com.jude.test.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
@@ -8,8 +9,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
  */
 public class App {
     public static void main(String[] args) {
-
-        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/jude/test/beans/beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         Person person = (Person) context.getBean("person");
         person.speak();
