d1d98d1eaba2cdd05f4370a7934adc20e6a1c282
jude <jude@gogame.net>
2015-10-04 13:47:57 +0800

    wiring the bean person to be used in the App

--- spring-tutorial-5/src/main/java/com/jude/spring/test/App.java
+++ spring-tutorial-5/src/main/java/com/jude/spring/test/App.java
@@ -3,9 +3,20 @@
  */
 package com.jude.spring.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
-        Person person = new Person();
+        /*
+        this is where we will tell context where the bean is
+         */
+        ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+        /*
+        this is where we get the bean and also in such a case we will need to cast the context
+        to the person type so that it can output it as a singleton
+         */
+        Person person = (Person) context.getBean("person");
         person.speak();
     }
 }
