131a5f264220885e57bfdaade6b59fc9c72c2154
jude <jude@gogame.net>
2015-10-04 17:47:49 +0800

    reinitialize as pervious commit was configured wrongly

---
+++ spring-tutorial-6/src/main/java/com/jude/test/App.java
@@ -0,0 +1,11 @@
+package com.jude.test;
+
+/**
+ * Created by judetan on 4/10/15.
+ */
+public class App {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.speak();
+    }
+}
--- spring-tutorial-6/src/main/java/com/jude/test/Person.java
+++ spring-tutorial-6/src/main/java/com/jude/test/Person.java
@@ -1,10 +1,10 @@
-package com.jude.spring.test;
+package com.jude.test;
 
 /**
  * Created by judetan on 4/10/15.
  */
 public class Person {
     public void speak() {
-        System.out.println("Hello i am a person.");
+        System.out.println("Hello i am a person");
     }
 }
