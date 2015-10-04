6d40a295098fc24592fee9b804a86c7e6dcd0f2c
jude <jude@gogame.net>
2015-10-04 19:18:52 +0800

    initialize tutorial 7

--- spring-tutorial-7/src/main/java/com/jude/spring/test/App.java
+++ spring-tutorial-7/src/main/java/com/jude/spring/test/App.java
@@ -1,4 +1,4 @@
-package com.jude.test.spring;
+package com.jude.spring.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
