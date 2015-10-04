4445a738df141e2fed47c3cd663d997e57cba906
jude <jude@gogame.net>
2015-10-04 13:28:19 +0800

    this is how we make a java program without spring

---
+++ spring-tutorial-5/src/main/java/com/jude/spring/test/App.java
@@ -0,0 +1,11 @@
+/**
+ * Created by judetan on 4/10/15.
+ */
+package com.jude.spring.test;
+
+public class App {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.speak();
+    }
+}
